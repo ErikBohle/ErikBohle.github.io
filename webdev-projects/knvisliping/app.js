f2dc2028de4099d3e0ad9c01a9d5102954fb1e3f
erikbohle <55728992+ErikBohle@users.noreply.github.com>
2020-05-22 20:36:24 +0200

    New project: Knifesharpening

---
+++ webdev-projects/knvisliping/app.js
@@ -0,0 +1,9 @@
+function openNav() {
+  document.getElementById("sidenav").style.width = "9em";
+  // document.getElementById("main").style.marginLeft = "52em";
+}
+
+function closeNav() {
+  document.getElementById("sidenav").style.width = "0em";
+  // document.getElementById("main").style.margin = "0em";
+} 
