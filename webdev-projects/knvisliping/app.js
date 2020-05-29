6b0ff83d721edbafaea475794b52270d65b24a3e
erikbohle <55728992+ErikBohle@users.noreply.github.com>
2020-05-29 21:32:40 +0200

    Rearranged to fit knifesharpening website

--- webdev-projects/knvisliping/app.js
+++
@@ -1,9 +0,0 @@
-function openNav() {
-  document.getElementById("sidenav").style.width = "10em";
-  // document.getElementById("main").style.marginLeft = "52em";
-}
-
-function closeNav() {
-  document.getElementById("sidenav").style.width = "0em";
-  // document.getElementById("main").style.margin = "0em";
-} 
